2f2d16317444cc34d54f6498cbc370cfd81e54dd
priyanshu1125-stack <priyanshu1125kumar@gmail.com>
2024-11-10 20:12:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log("hello i am conditional tutorial")
+ let age=3;
+ let grace=2;
+//  console.log(age+grace)
+//  console.log(age-grace)
+//  console.log(age*grace)
+//  console.log(age**grace)
+//  console.log(age/grace)
+//  console.log(age%grace)
+
+ if(age>18) {
+    console.log("you can drive");
+ }
+ else if(age==18)  {
+    console.log("you are ready to drive");
+ }
+ 
+  else {
+    console.log("you can not drive")
+  }
+  a=6;
+  b=8;
+  let c=a>b ? (a-b):(b-a);
+  /*
+  translate to:
+  if(a>b) {
+  let c=a-b;
+  }
+  else {
+  let c=a-b;
+} */
